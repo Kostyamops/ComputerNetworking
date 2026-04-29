3f507e71760eb032eacd4ebb929749a628a7fb7e
Konstantin Gritzkevich <kgritz@yandex.ru>
2026-04-29 12:58:16 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,14 +2,14 @@
 
 Get-NetAdapter | select Name, InterfaceDescription, Status, Speed | ft -AutoSize
 
-$n = Read-Host "Введите имя адаптера (из колонки Name)"
+$n = Read-Host "Enter Adapter Name"
 
 $m = Read-Host "1 - DHCP, 2 - Static"
 
 if ($m -eq "1") {
     Set-NetIPInterface -InterfaceAlias $n -DHCP Enabled
     Set-DnsClientServerAddress -InterfaceAlias $n -ResetServerAddresses
-    Write-Host "OK: Включен DHCP"
+    Write-Host "OK: DHCP is ON"
 } else {
     $ip = Read-Host "IP"
     $gw = Read-Host "Gateway"
@@ -17,6 +17,6 @@ if ($m -eq "1") {
     
     New-NetIPAddress -InterfaceAlias $n -IPAddress $ip -PrefixLength 24 -DefaultGateway $gw
     Set-DnsClientServerAddress -InterfaceAlias $n -ServerAddresses $dns
-    Write-Host "OK: Статика настроена"
+    Write-Host "OK: Static IP is set"
 }
 pause
